335c200d12827b57d58ccd57a06c01dfd89e1981
AnaG-10 <a.garg032005@gmail.com>
2026-09-24 16:24:18 +0530

    frontend added

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-
+from fastapi.middleware.cors import CORSMiddleware
 from typing import Literal
 
 from fastapi import FastAPI
@@ -11,6 +11,16 @@ app = FastAPI(
     title="FitMind AI",
     description="AI-powered fashion recommendation API",
     version="1.0.0"
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
